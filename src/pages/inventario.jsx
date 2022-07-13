ece1b58a56006e5223a61615e2f0f41148ee9c41
Adrian1jr <adrian1jr@gmail.com>
2022-07-12 20:55:59 -0500

    UPDATE pestaña inventario

--- src/pages/inventario.jsx
+++ src/pages/inventario.jsx
@@ -1,9 +1,30 @@
 import ResponsiveAppBar from "../components/NavBar";
 
+const arrayOfComponents = [
+  {
+    id: 1,
+    name: "Inicio",
+  },
+  {
+    id: 2,
+    name: "Inventario",
+  },
+  {
+    id: 3,
+    name: "Reportes",
+  },
+];
+
 export const Inventario = () => {
   return (
     <div>
       <ResponsiveAppBar />
+
+      <div>
+        {arrayOfComponents.map((comp) => (
+          <h1 key={comp.id}>{comp.name}</h1>
+        ))}
+      </div>
     </div>
   );
 };
